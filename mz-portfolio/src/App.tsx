4bec05e19729dd7e62ecb96bc4da28a76d5ef8c6
Wulthus <sickness2024@protonmail.com>
2024-06-19 19:36:39 +0200

    Initial setup for my portfolio website, a blank slate.

---
+++ mz-portfolio/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { BlankSlate } from './components/blank-slate-message/blank-slate';
+
+export function App() {
+  return (
+    <div>
+      <BlankSlate></BlankSlate>
+    </div>
+  )
+}
---
+++ mz-portfolio/src/components/blank-slate-message/blank-slate.tsx
@@ -0,0 +1,3 @@
+export function BlankSlate(){
+    return <h1 className="blank-slate">Blank Slate</h1>
+}
